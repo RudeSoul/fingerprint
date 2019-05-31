95cf8b2c50a2ebba6d6cab710ad5ad91a5cd6440
Prabesh <prabesh7@gmail.com>
2019-05-31 21:07:00 +0545

    commit

---
+++ details.php
@@ -0,0 +1,32 @@
+<?php 
+  session_start(); 
+
+  if (!isset($_SESSION['username'])) {
+    $_SESSION['msg'] = "You must log in first";
+    header('location: login.php');
+  }
+  if (isset($_GET['logout'])) {
+    session_destroy();
+    unset($_SESSION['username']);
+    header("location: login.php");
+  }
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Home</title>
+  <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Gradient Background</title>
+    <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+ <h1><a href="index.html">Details? Get The hell our of here.</a></h1>
+ <?php  if (isset($_SESSION['username'])) : ?>
+  <a href="index.php?logout='1'"><button class="btn"><i class="fa fa-close"></i></button>
+    <?php endif ?>
+    </a>     
+</body>
+</html>
